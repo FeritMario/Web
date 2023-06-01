c800ba76d831a93a37cd2f3ef3b91b506408c731
Mario <hardimario13@gmail.com>
2023-06-01 11:32:50 +0200

    Test

---
+++ API/API/Models/Restaurant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Models
+{
+    public class Restaurant
+    {
+        public Guid? Id { get; set; }
+        public int Rate { get; set; }
+        public string Name { get; set; }
+    }
+}
